96115c07704a2817c4c057828b9f4e63d6424da3
Trupti0701 <truptibhilare0701>
2025-05-29 10:42:37 +0530

    first commit

---
+++ traingleArea.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 11 09:16:10 2025
+
+@author: Admin
+"""
+
+base=int(input("Enter base"))
+height=int(input("Enter height"))
+area=0.5*base*height
+print("area of triangle is",area)
